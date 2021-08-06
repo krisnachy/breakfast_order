80f937ef4cd5e1194ef46d4324519f19ef46cf06
krisnachy <cahyayuana@gmail.com>
2021-08-06 09:53:25 +0700

    Add Splashscreen

---
+++ lib/splashscreen.dart
@@ -0,0 +1,28 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:order_breakfast/main.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreen createState() => _SplashScreen();
+}
+
+class _SplashScreen extends State<SplashScreen> {
+  void initState() {
+    super.initState();
+    startSplashScreen();
+  }
+
+  startSplashScreen() async {
+    var duration = const Duration(seconds: 3);
+    return Timer(duration, () {
+      Navigator.push(context, MaterialPageRoute(builder: (context) => MyApp()));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
